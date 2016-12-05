cc3567d6b7f4eee65b1de8ac32a82d29da0e7bb1
Broulaye <broulaye@vt.edu>
2016-12-04 20:54:43 -0500

    testing server

--- src/webService.c
+++ src/webService.c
@@ -1,69 +1,16 @@
-#include <stdio.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
+#include "csapp.h"
+#include "list.h"
 #include <arpa/inet.h>
-#include <unistd.h>
-#include <strings.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <pthread.h>
-#include <sys/wait.h>
-#include <stdlib.h>
 
-#define RIO_BUFSIZE 8192
-#define MAXLINE 4096
-typedef struct {
-    int rio_fd; /* Descriptor for this internal buf */
-    int rio_cnt; /* Unread bytes in internal buf */
-    char *rio_bufptr; /* Next unread byte in internal buf */
-    char rio_buf[RIO_BUFSIZE]; /* Internal buffer */
-} rio_t;
 int start_server(short*);
 void process_request(int);
 void usage(char*);
-ssize_t rio_readn(int, void *, size_t);
-ssize_t rio_writen(int, void *, size_t);
-ssize_t rio_readlineb(rio_t*, void*, size_t);
-void rio_readinitb(rio_t*, int);
 void read_requesthdrs(rio_t *);
 int parse_uri(char *, char *, char *);
 void serve_static(int,char *,int);
 void get_filetype(char *, char *);
 void serve_dynamic(int, char *, char *);
 void clienterror(int, char *, char *,char *, char *);
-
-
-void rio_readinitb(rio_t *rp, int fd) {
-    rp->rio_fd = fd;
-    rp->rio_cnt = 0;
-    rp->rio_bufptr = rp->rio_buf;
-}
-
-
-ssize_t rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen){
-    int n, rc;
-    char c, *bufp = usrbuf;
-
-    for (n = 1; n < maxlen; n++) {
-        if ((rc = rio_read(rp, &c, 1)) == 1) {
-            *bufp++ = c;
-            if (c == ’\n’)
-                break;
-        }
-        else if (rc == 0) {
-            if (n == 1)
-                return 0; /* EOF, no data read */
-            else
-                break; /* EOF, some data was read */
-        }
-        else
-            return -1; /* Error */
-    }
-    *bufp = 0;
-
-    return n;
-}
 
 
 void process_request(int client_fd) {
@@ -79,7 +26,7 @@ void process_request(int client_fd) {
 
     sscanf(buf, "%s %s %s", method, uri, version);
     if (strcasecmp(method, "GET")) {
-        clienterror(fd, method, "501", "Not Implemented",
+        clienterror(client_fd, method, "501", "Not Implemented",
             "Tiny does not implement this method");
         return;
     }
@@ -184,15 +131,15 @@ int parse_uri(char *uri, char *filename, char *cgiargs)
         strcpy(cgiargs, "");
         strcpy(filename, ".");
         strcat(filename, uri);
-        if (uri[strlen(uri)-1] == ’/’)
+        if (uri[strlen(uri)-1] == '/')
             strcat(filename, "home.html");
         return 1;
     }
     else { /* Dynamic content */
-        ptr = index(uri, ’?’);
+        ptr = index(uri, '?');
         if (ptr) {
             strcpy(cgiargs, ptr+1);
-            *ptr = ’\0’;
+            *ptr = '\0';
         }
         else
             strcpy(cgiargs, "");
@@ -208,7 +155,6 @@ void serve_static(int fd, char *filename, int filesize)
 {
     int srcfd;
     char *srcp, filetype[MAXLINE], buf[MAXBUF];
-
     /* Send response headers to client */
     get_filetype(filename, filetype);
     sprintf(buf, "HTTP/1.0 200 OK\r\n");
@@ -259,54 +205,13 @@ void serve_dynamic(int fd, char *filename, char *cgiargs)
     wait(NULL); /* Parent waits for and reaps child */
 }
 
-
-ssize_t rio_readn(int fd, void *usrbuf, size_t n) {
-     size_t nleft = n;
-     ssize_t nread;
-     char *bufp = usrbuf;
-
-     while (nleft > 0) {
-         if ((nread = read(fd, bufp, nleft)) < 0) {
-             if (errno == EINTR) /* interrupted by sig handler return */
-             nread = 0; /* and call read() again */
-             else
-             return -1; /* errno set by read() */
-         }
-         else if (nread == 0)
-            break; /* EOF */
-         nleft -= nread;
-         bufp += nread;
-     }
-
-     return (n - nleft); /* return >= 0 */
-}
-
-ssize_t rio_writen(int fd, void *usrbuf, size_t n) {
-     size_t nleft = n;
-     ssize_t nwritten;
-     char *bufp = usrbuf;
-
-     while (nleft > 0) {
-         if ((nwritten = write(fd, bufp, nleft)) <= 0) {
-             if (errno == EINTR) /* interrupted by sig handler return */
-                nwritten = 0; /* and call write() again */
-             else
-                return -1; /* errno set by write() */
-         }
-         nleft -= nwritten;
-         bufp += nwritten;
-     }
-     return n;
-}
-
-
 int start_server(short *port) {
    int socket, optval=1;
    struct sockaddr_in server_addr;
 
 
    /* First call to socket() function */
-   socket = socket(AF_INET, SOCK_STREAM, 0);
+   socket = Socket(AF_INET, SOCK_STREAM, 0);
 
    if (socket < 0) {
       perror("ERROR opening socket");
@@ -315,7 +220,7 @@ int start_server(short *port) {
 
 
     /* Eliminates "Address already in use" error from bind */
-     if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR,
+     if (setsockopt(socket, SOL_SOCKET, SO_REUSEADDR,
      (const void *)&optval , sizeof(int)) < 0)
         return -1;
 
@@ -324,8 +229,8 @@ int start_server(short *port) {
    bzero((char *) &server_addr, sizeof(server_addr));
 
    server_addr.sin_family = AF_INET;
-   server_addr.sin_addr.s_addr = htlon(INADDR_ANY);
-   server_addr.sin_port = htons(*port);
+   server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+   server_addr.sin_port = htons(port);
 
    /* Now bind the host address using bind() call.*/
    if (bind(socket, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
@@ -355,25 +260,22 @@ void usage(char *av0) {
 
 int main(int ac, char** av)
 {
-    unsigned short port;
-    struct sockaddr_in client_addr;
-    int client_fd, client_size, socket;
-    char *path
+    short port;
+    struct sockaddr client_addr;
+    int client_fd, socket;
+    socklen_t client_size;
+    //char *path;
     int c;
-    while ((c = getopt(ac, av, "p:R:")) != EOF) {
+    while ((c = getopt(ac, av, "p:")) != EOF) {
         switch (c) {
         case 'p':
             port = atoi(optarg);
             break;
-        case 'R':
-            path = optarg;
-            break;
-
         }
     }
     if (optind == ac)
         usage(av[0]);
-    socket = start_server(*port);
+    socket = start_server(&port);
     while(1) {
         client_size = sizeof(client_addr);
 
